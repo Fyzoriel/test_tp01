ae661aa94433f11fa91c7df0f56be92e8106e8d2
Fyzoriel.le | leo <leoseguin8+github@gmail.com>
2023-02-21 10:56:14 +0100

    feat: RED - add greater than 150 failing scenario

--- TestFizzBuzz/Test.cs
+++ TestFizzBuzz/Test.cs
@@ -12,4 +12,15 @@ public class UnitTest1
         // Act & Assert
         Assert.Throws<ArgumentException>(() => fizzBuzz.GetFizzBuzz(number));
     }
+    
+    [Fact]
+    public void GreaterThan150()
+    {
+        // Arrange
+        var fizzBuzz = new FizzBuzz.FizzBuzz();
+        const int number = 151;
+        
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => fizzBuzz.GetFizzBuzz(number));
+    }
 }
