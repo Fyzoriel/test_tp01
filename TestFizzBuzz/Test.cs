4a78be8b88a0ea09c33904da7947b152f4c03ad5
Fyzoriel.le | leo <leoseguin8+github@gmail.com>
2023-02-21 11:14:02 +0100

    feat: RED - FizzBuzz

--- TestFizzBuzz/Test.cs
+++ TestFizzBuzz/Test.cs
@@ -3,6 +3,21 @@ namespace TestFizzBuzz;
 public class UnitTest1
 {
     [Fact]
+    public void FizzBuzz()
+    {
+        // Arrange
+        var fizzBuzz = new FizzBuzz.FizzBuzz();
+        const int number = 15;
+        
+        // Act
+        var result = fizzBuzz.GetFizzBuzz(number);
+        
+        // Assert
+        const string expected = "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz";
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
     public void LessThan15()
     {
         // Arrange
