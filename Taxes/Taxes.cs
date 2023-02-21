e0e66e4e4fbed21185920b40d9460deb2ea56d8c
Fyzoriel.le | leo <leoseguin8+github@gmail.com>
2023-02-21 13:23:47 +0100

    feat: GREEN : add 0% taxes

--- Taxes/Taxes.cs
+++ Taxes/Taxes.cs
@@ -5,6 +5,11 @@ public class Taxes
     public string CalculateTax(int income)
     {
         ValidateIncome(income);
+        if (income <= 10777)
+        {
+            return "0% - 0";
+        }
+        
         throw new NotImplementedException();
     }
     
