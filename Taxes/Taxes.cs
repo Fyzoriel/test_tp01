a4920431533bb6ef3db62ab4a56eaf5aa4b01b0d
Fyzoriel.le | leo <leoseguin8+github@gmail.com>
2023-02-21 13:37:01 +0100

    feat: GREEN : add 11% taxes

--- Taxes/Taxes.cs
+++ Taxes/Taxes.cs
@@ -9,6 +9,10 @@ public class Taxes
         {
             return "0% - 0";
         }
+        if (income <= 27478)
+        {
+            return $"11% - {income * 0.11}";
+        }
         
         throw new NotImplementedException();
     }
