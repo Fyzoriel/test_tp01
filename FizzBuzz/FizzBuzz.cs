73e97062c6e187edb6963abf8f7c80ba437a8e57
Fyzoriel.le | leo <leoseguin8+github@gmail.com>
2023-02-21 10:36:16 +0100

    feat: GREEN - handle number less than 15

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -4,6 +4,10 @@ public class FizzBuzz
 {
     public string GetFizzBuzz(int number)
     {
+        if (number < 15)
+        {
+            throw new System.ArgumentException("Number must be greater than 15");
+        }
         throw new System.NotImplementedException();
     }
 }
