e6cfbe24164246a44c05c99a8df039ad3846693b
Fyzoriel.le | leo <leoseguin8+github@gmail.com>
2023-02-21 12:19:39 +0100

    refactor: add validation method

--- Taxes/Taxes.cs
+++ Taxes/Taxes.cs
@@ -4,10 +4,15 @@ public class Taxes
 {
     public decimal CalculateTax(int income)
     {
+        ValidateIncome(income);
+        throw new NotImplementedException();
+    }
+    
+    public void ValidateIncome(int income)
+    {
         if (income < 0)
         {
-            throw new ArgumentException("Income cannot be negative", nameof(income));
+            throw new ArgumentException("Income cannot be negative");
         }
-        throw new NotImplementedException();
     }
 }
