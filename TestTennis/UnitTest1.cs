1e8967e2a614e8d9b6b335d5429acddc0373b8b6
Fyzoriel.le | leo <leoseguin8+github@gmail.com>
2023-02-22 10:56:53 +0100

    feat: add more cases

--- TestTennis/UnitTest1.cs
+++ TestTennis/UnitTest1.cs
@@ -7,6 +7,8 @@ public class UnitTest1
     [Theory]
     [InlineData(0, 0, "Love - Love")]
     [InlineData(1, 0, "Fifteen - Love")]
+    [InlineData(0, 1, "Love - Fifteen")]
+    [InlineData(1,1, "Fifteen - Fifteen")]
     public void TestScore(int playerOneBall, int playerTwoBall, string expectedScore)
     {
         // Arrange
