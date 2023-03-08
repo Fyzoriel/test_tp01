613e3ca92e521e50338592666669a029770b8f6e
Fyzoriel.le | leo <leoseguin8+github@gmail.com>
2023-03-08 16:36:04 +0100

    feat: total loan interest

--- TP2/TP2/RealEstateLoanCalculator.cs
+++ TP2/TP2/RealEstateLoanCalculator.cs
@@ -18,4 +18,9 @@ public class RealEstateLoanCalculator
         double monthlyInterestRate = _interestRate / 12;
         return Math.Round((_loanAmount * monthlyInterestRate) / (1 - Math.Pow(1 + monthlyInterestRate, -_loanTermInMonths)), 2);
     }
+    
+    public double CalculateTotalInterest()
+    {
+        return Math.Round(CalculateMonthlyPayment() * _loanTermInMonths - _loanAmount, 2);
+    }
 }
--- TP2/Test/Test.cs
+++ TP2/Test/Test.cs
@@ -7,7 +7,8 @@ public class UnitTest1
     [Theory]
     [InlineData(50000, 0.03, 300, 237.11)]
     [InlineData(100000, 0.03, 300, 474.21)]
-    public void ItShouldReturnCorrectRealEstateMonthlyPayment(int loanAmount, double interestRate, int loanTermInMonths, double expectedMonthlyPayment)
+    public void ItShouldReturnCorrectRealEstateMonthlyPayment(int loanAmount, double interestRate, int loanTermInMonths,
+        double expectedMonthlyPayment)
     {
         // Arrange
         var realEstateLoanCalculator = new RealEstateLoanCalculator(loanAmount, interestRate, loanTermInMonths);
@@ -28,10 +29,11 @@ public class UnitTest1
     [InlineData(50000, false, false, false, false, true, 18.75)]
     [InlineData(50000, true, true, true, true, true, 33.33)]
     public void ItShouldReturnCorrectMonthlyInsurancePayment(
-        int loanAmount, bool isSporty, bool isSmoker, bool hasHeartCondition, 
+        int loanAmount, bool isSporty, bool isSmoker, bool hasHeartCondition,
         bool isComputerEngineer, bool isFighterPilot, double expected)
     {
-        var monthlyInsurancePayment = new MonthlyInsurancePayment(loanAmount, isSporty, isSmoker, hasHeartCondition, isComputerEngineer, isFighterPilot);
+        var monthlyInsurancePayment = new MonthlyInsurancePayment(loanAmount, isSporty, isSmoker, hasHeartCondition,
+            isComputerEngineer, isFighterPilot);
         Assert.Equal(expected, monthlyInsurancePayment.CalculateMonthlyInsurancePayment());
     }
     
@@ -45,7 +47,8 @@ public class UnitTest1
     {
         // Arrange
         var realEstateLoanCalculator = new RealEstateLoanCalculator(loanAmount, interestRate, loanTermInMonths);
-        var monthlyInsurancePayment = new MonthlyInsurancePayment(loanAmount, isSporty, isSmoker, hasHeartCondition, isComputerEngineer, isFighterPilot);
+        var monthlyInsurancePayment = new MonthlyInsurancePayment(loanAmount, isSporty, isSmoker, hasHeartCondition,
+            isComputerEngineer, isFighterPilot);
         var loanCalculator = new LoanCalculator(realEstateLoanCalculator, monthlyInsurancePayment);
 
         // Act
@@ -53,5 +56,20 @@ public class UnitTest1
         
         // Assert
         Assert.Equal(expected, monthlyPayment);
+    }
+
+    [Theory]
+    [InlineData(50000, 0.03, 300, 21133)]
+    public void ItShouldReturnCorrectTotalLoanInterest(int loanAmount, double interestRate, int loanTermInMonths,
+        double expected)
+    {
+        // Arrange
+        var realEstateLoanCalculator = new RealEstateLoanCalculator(loanAmount, interestRate, loanTermInMonths);
+        
+        // Act
+        var totalLoanInterest = realEstateLoanCalculator.CalculateTotalInterest();
+        
+        // Assert
+        Assert.Equal(expected, totalLoanInterest);
     }
 }
