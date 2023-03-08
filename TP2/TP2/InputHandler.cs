06cbec96fa64f32315c58a75c08585a747e1b035
Fyzoriel.le | leo <leoseguin8+github@gmail.com>
2023-03-08 19:29:04 +0100

    feat: Program, input handler

---
+++ TP2/TP2/InputHandler.cs
@@ -0,0 +1,78 @@
+﻿namespace TP2;
+
+public class InputHandler
+{
+    private static T GetValidInput<T>(string prompt, Func<string, bool> validateFunc, Func<string, T> convertFunc)
+    {
+        while (true)
+        {
+            Console.Write(prompt);
+            var input = Console.ReadLine();
+            if (input != null && validateFunc(input))
+            {
+                return convertFunc(input);
+            }
+            Console.WriteLine("Invalid input. Please try again.");
+        }
+    }
+    
+    public static bool GetBoolInput(string prompt)
+    {
+        return GetValidInput(prompt, input => input == "y" || input == "n", input => input == "y");
+    }
+    
+    public static double GetDoubleInput(string prompt)
+    {
+        return GetValidInput(prompt, input => double.TryParse(input, out _), double.Parse);
+    }
+
+    public static LoanAmount GetLoanAmountInput(string prompt)
+    {
+        return GetValidInput(
+            prompt,
+            input =>
+            {
+                if (!int.TryParse(input, out int amount))
+                {
+                    return false;
+                }
+
+                try
+                {
+                    _ = new LoanAmount(amount);
+                    return true;
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return false;
+                }
+            },
+            int.Parse);
+    }
+    
+    public static LoanTermInMonths GetLoanTermInMonthsInput(string prompt)
+    {
+        return GetValidInput(
+            prompt,
+            input =>
+            {
+                if (!int.TryParse(input, out int amount))
+                {
+                    return false;
+                }
+
+                try
+                {
+                    _ = new LoanTermInMonths(amount);
+                    return true;
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return false;
+                }
+            },
+            int.Parse);
+    }
+}
--- TP2/TP2/Program.cs
+++ TP2/TP2/Program.cs
@@ -1,3 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using TP2;
 
-Console.WriteLine("Hello, World!");
+var loanAmount = InputHandler.GetLoanAmountInput("Enter loan amount: ");
+var loanTermInMonths = InputHandler.GetLoanTermInMonthsInput("Enter loan term in months: ");
+var interestRate = InputHandler.GetDoubleInput("Enter interest rate: ");
+
+var isSporty = InputHandler.GetBoolInput("Are you sporty? (y/n): ");
+var isSmoker = InputHandler.GetBoolInput("Are you a smoker? (y/n): ");
+var hasHeartCondition = InputHandler.GetBoolInput("Do you have a heart condition? (y/n): ");
+var isComputerEngineer = InputHandler.GetBoolInput("Are you a computer engineer? (y/n): ");
+var isFighterPilot = InputHandler.GetBoolInput("Are you a fighter pilot? (y/n): ");
+
+Console.WriteLine(interestRate);
+
+var loanData = new LoanData(loanAmount, loanTermInMonths);
+var realEstateLoanCalculator = new RealEstateLoanCalculator(loanData, interestRate);
+
+var insurancePayment = new InsurancePayment(loanData, isSporty, isSmoker, hasHeartCondition,
+    isComputerEngineer, isFighterPilot);
+
+var loanCalculator = new LoanCalculator(realEstateLoanCalculator, insurancePayment);
+
+var monthlyPayment = loanCalculator.CalculateMonthlyPayment();
+var monthlyInsurancePayment = insurancePayment.CalculateMonthlyInsurancePayment();
+var totalLoanInterest = realEstateLoanCalculator.CalculateTotalInterest();
+var totalInsurancePayment = insurancePayment.CalculateTotalInsurancePayment();
+
+Console.WriteLine($"Monthly payment: {monthlyPayment}");
+Console.WriteLine($"Monthly insurance payment: {monthlyInsurancePayment}");
+Console.WriteLine($"Total monthly payment: {monthlyPayment + monthlyInsurancePayment}");
+
+Console.WriteLine($"Total loan interest: {totalLoanInterest}");
+Console.WriteLine($"Total insurance payment: {totalInsurancePayment}");
+Console.WriteLine($"Total loan interest + total insurance payment: {totalLoanInterest + totalInsurancePayment}");
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
